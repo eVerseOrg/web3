46f966a3ff26d11b104de0a7f750811dfd57ded4
Felix Baum <felix@everse.tv>
2022-05-31 10:37:32 -0700

    connect-wallet initial commit

---
+++ connect-wallet/components/ConnectWallet.tsx
@@ -0,0 +1,47 @@
+import { useGnosis } from "@thirdweb-dev/react";
+import {
+  useMetamask,
+  useWalletConnect,
+  useCoinbaseWallet,
+  useNetwork,
+  useAddress,
+  useDisconnect,
+} from '@thirdweb-dev/react';
+
+
+export const ConnectWallet = () => {
+  const connectWithCoinbaseWallet = useCoinbaseWallet();
+  const connectWithMetamask = useMetamask();
+  const connectWithWalletConnect = useWalletConnect();
+  const connectWithGnosis = useGnosis();
+  const disconnectWallet = useDisconnect();
+  const address = useAddress();
+  const network = useNetwork();
+
+  // If a wallet is connected, show address, chainId and disconnect button
+  if (address) {
+    return (
+      <div>
+        Address: {address}
+        <br />
+        Chain ID: {network[0].data.chain && network[0].data.chain.id}
+        <br />
+        <button onClick={disconnectWallet}>Disconnect</button>
+      </div>
+    );
+  }
+
+  // If no wallet is connected, show connect wallet options
+  return (
+    <div>
+      <button onClick={() => connectWithCoinbaseWallet()}>
+        Connect Coinbase Wallet
+      </button>
+      <button onClick={() => connectWithMetamask()}>Connect MetaMask</button>
+      <button onClick={() => connectWithWalletConnect()}>
+        Connect WalletConnect
+      </button>
+      <button onClick={() => connectWithGnosis()}>Connect Gnosis</button>
+    </div>
+  );
+};
